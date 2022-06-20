ff239f12e4301f271b7da9bf79e9cd660e5f6ea0
eze-etchecoin <eze.etchecoin.92@gmail.com>
2022-06-20 19:05:33 -0300

    agregamos validacion para 5 números por fila

--- BingoGenerator/BingoGenerator/Program.cs
+++ BingoGenerator/BingoGenerator/Program.cs
@@ -1,10 +1,12 @@
 ﻿/*
 Ejercicio: generar un cartón válido para jugar al bingo. Las condiciones son:
-1 - Quince números del 1 al 89 inclusive
-2 - Un cartón de 9 columnas x 3 filas
-3 - Cada columna contiene 1 decena particular. Por ejemplo, la 1ra columna puede contener números del 1 al 9 inclusive,
-    la 2da del 10 al 19 inclusive, etc.
-4 - 
+1 - Cartón de 3 filas por 9 columnas
+2 - El cartón debe tener 15 números y 12 espacios en blanco
+3 - Cada fila debe tener 5 números
+4 - Cada columna debe tener 1 o 2 números
+5 - Ningún número puede repetirse
+6 - La primer columna contiene los números del 1 al 9, la segunda del 10 al 19, la tercera del 20 al 29, 
+    así sucesivamente hasta la ultima columna la cual contiene del 80 al 90.
 */
 
 var numeros = new int[15];
@@ -16,7 +18,7 @@ for (int i = 0; i < numeros.Length; i++)
 {
     var candidato = random.Next(menorNumero, mayorNumero);
    
- if(i > 0)
+    if(i > 0)
     {
         bool esRepetido;
 
@@ -45,7 +47,7 @@ for (int i = 0; i < numeros.Length; i++)
                 decenasDelCandidato++;
             }
 
-            var numerosEnEstaDecena = 0;
+            var numerosEnEstaDecena = 1;
             var decenasDelNumeroExistente = 0;
             for (int j = 0; j < i; j++)
             {
@@ -59,6 +61,8 @@ for (int i = 0; i < numeros.Length; i++)
                 }
                 if (decenasDelNumeroExistente == decenasDelCandidato)
                     numerosEnEstaDecena++;
+
+                decenasDelNumeroExistente = 0;
             }
             if (numerosEnEstaDecena < 3)
             {
@@ -66,7 +70,7 @@ for (int i = 0; i < numeros.Length; i++)
             }
             else
             {
-                candidato = random.Next(1, 90);
+                candidato = random.Next(menorNumero, mayorNumero);
             }
         }
         while (numeros[i] == 0);
@@ -108,15 +112,32 @@ for (int i = 0; i < numeros.Length; i++)
     var columna = decenas;
     var filaAleatoria = random.Next(3);
 
+    var cantVueltas = 0;
     do
     {
-        if (string.IsNullOrEmpty(carton[columna, filaAleatoria]))
+        //Verificamos que en la fila haya menos de 5 números
+        var cantNumerosEnFila = 0;
+        for (int j = 0; j < 9; j++)
+        {
+            if (!string.IsNullOrEmpty(carton[j, filaAleatoria]))
+            {
+                cantNumerosEnFila++;
+            }
+        }
+
+        if (string.IsNullOrEmpty(carton[columna, filaAleatoria]) && cantNumerosEnFila < 5)
         {
             carton[columna, filaAleatoria] = numeros[i].ToString().PadLeft(2, '0');
         }
         else
         {
             filaAleatoria = random.Next(3);
+        }
+
+        cantVueltas++;
+        if(cantVueltas > 1000)
+        {
+            throw new ApplicationException("Bucle infinito. Mala suerte :(");
         }
     }
     while (string.IsNullOrEmpty(carton[columna, filaAleatoria]) || 
